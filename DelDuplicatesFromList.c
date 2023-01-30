576b5502f37cfcfcc50ff953403d98defea98722
reberh00 <115488806+reberh00@users.noreply.github.com>
2023-01-30 13:23:14 +0100

    Update DelDuplicatesFromList.c

--- DelDuplicatesFromList.c
+++ DelDuplicatesFromList.c
@@ -1,15 +1,15 @@
-//Napisati program koji generira 20 sluèajnih brojeva u opsegu od 10 - 20 i sprema ih u vezanu listu, po
+//Napisati program koji generira 20 slučajnih brojeva u opsegu od 10 - 20 i sprema ih u vezanu listu, po
 //redoslijedu generiranja, a ne sortirano.
 //a) Korisnik unosi jedan broj i potrebno je iz vezane liste izbrisati sva pojavljivanja tog broja i
 //ispisati vezanu listu.
 //b) Potrebno je izbrisati sve duple vrijednosti i ispisati vezanu listu.
 
-#define _CRT_SECURE_NO_WARNIGNS
-
-
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
+#include<ctype.h>
 
 
 struct _element;
@@ -20,11 +20,12 @@ struct _element {
 	position next;
 };
 
-int Add_Number(position);
-int Delete_List(position);
-int Print_List(position);
-int Delete_Number(position, int);
-int Delete_Duplicates(position);
+int addList(position);
+void delAll(position);
+void printList(position);
+void delNumber(position, int);
+void delDuplicates(position);
+
 
 int main() {
 	element head;
@@ -35,84 +36,76 @@ int main() {
 
 	srand((unsigned)time(NULL));
 
-	result = Add_Number(&head);
+	result = addList(&head);
 	if (result != 0) {
-		result = Delete_List(&head);
+		delAll(&head);
 		return 0;
 	}
-	printf("Random lista:\n");
-	result = Print_List(head.next);
 
+	printf("DODANA RANDOM LISTA:\n");
+	printList(head.next);
 
-	printf("Unesi broj koji bi izbacia iz liste:\n");
+	printf("Unesi broj koji zelis izbrisat iz liste:\n");
 	scanf("%d", &choice);
 	
-	result = Delete_Number(&head, choice);
-	if (result != 0) {
-		result = Delete_List(&head);
-		return 0;
-	}
-	printf("Lista bez broja %d:\n", choice);
-	result = Print_List(head.next);
-
-	printf("Lista bez duplih brojeva:\n");
-	result = Delete_Duplicates(&head);
-	if (result != 0) {
-		result = Delete_List(&head);
-		return 0;
-	}
-	result = Print_List(head.next);
-
-	result = Delete_List(&head);
+	delNumber(&head, choice);
+	
+	printf("LISTA BEZ %d:\n", choice);
+	printList(head.next);
 
 
+	delDuplicates(&head);
+	printf("Lista bez duplikata:\n");
+	printList(head.next);
+
+	delAll(&head);
+	printList(head.next);
 
 	return 0;
 }
-int Add_Number(position p) {
-	position new_element = NULL;
+int addList(position p) {
+	position ne = NULL;
 
 	for (int i = 0; i < 20; i++) {
 
-		new_element = (position)malloc(sizeof(element));
-		if (new_element == NULL) {
-			printf("Greska u dyn alloc\n");
+		ne = (position)malloc(sizeof(element));
+		if (ne == NULL) {
+			printf("greska u dyn alloc!");
 			return -1;
 		}
 
-		new_element->num = (rand() % (20 - 10 + 1)) + 10;
+		ne->num = (rand() % (20 - 10 + 1)) + 10;
 
-		new_element->next = NULL;
-		p->next = new_element;
+		ne->next = NULL;
+		p->next = ne;
 
 		p = p->next;
+
 	}
 
 	return 0;
 }
-int Delete_List(position p) {
+void delAll(position p) {
 	position temp = NULL;
 
 	while (p->next != NULL) {
 		temp = p->next;
 		p->next = temp->next;
 		free(temp);
-	}
-	printf("\n");
-	printf("Lista je izbrisana.\n");
 
-	return 0;
+	}
+
+	printf("lista je izbrisana!\n");
 }
-int Print_List(position p) {
+void printList(position p) {
 
 	while (p != NULL) {
-		printf("%d\n", p->num);
+		printf("%d\n",p->num);
 		p = p->next;
 	}
-
-	return 0;
+	printf("\n");
 }
-int Delete_Number(position p, int value) {
+void delNumber(position p, int value) {
 	position temp = NULL;
 
 	while (p->next != NULL) {
@@ -122,24 +115,22 @@ int Delete_Number(position p, int value) {
 			p->next = temp->next;
 			free(temp);
 		}
-		else {
+		else
 			p = p->next;
-		}
-		
 	}
 
-	return 0;
-}
-int Delete_Duplicates(position p) {
-	int result = 0;
 
-	p = p->next;
+	
+}
+void delDuplicates(position p) {
 
 	while (p != NULL) {
-		result = Delete_Number(p, p->num);
+		delNumber(p, p->num);
 		p = p->next;
 
 	}
 
-	return 0;
+
+
+	
 }
